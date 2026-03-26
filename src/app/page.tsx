20c668a2dcc234129f4063e10bc076def86c6057
ming <ming92035@gmail.com>
2026-03-26 22:40:47 +0900

    feat: #10 권한에 따른 헤더, 네브 표시 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import InputWithIcon from '../components/ui/input-with-icon/InputWithIcon';
-import { Search } from 'lucide-react';
-import { Dashboard } from '@/components/dashboard/Dashboard';
 import DashboardPage from '@/components/dashboard/page';
 
 export default function Home() {
